4dbf4c2913a7e52d66e851748e478c8c6d3cfc26
sridhar-padala <sridharseven17@gmail.com>
2026-02-02 20:48:00 +0530

    feat : integrate Clerk authentication and set up user routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,16 @@ import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import userRoutes from "./routes/userRoutes";
+import { clerkMiddleware } from '@clerk/express'
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 //middleware
 app.use(express.json());// parses incoming JSON req bodies and makes them available as req.body
 
+//clerk middleware
+app.use(clerkMiddleware());
 
 //health route
 app.get("/health",(req,res)=>{
@@ -22,6 +26,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/chat",chatRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+//error handlers must come after all the routes and other middlewares so they can catch
+//errors passed with next(err) or thrown inside async handlers.
+app.use(errorHandler);
 
 export default app;
 
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { getMe, authCallback } from "../controllers/authController";
+import { protectRoute } from "../middleware/auth";
 
 const router = Router()
+
+router.get("/me",protectRoute, getMe)
+router.post("/callback", authCallback);
 
 export default router;
